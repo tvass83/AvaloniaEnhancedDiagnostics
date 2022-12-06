000128f8ab4d683e7cd27e3e6f1fdddce1abd356
Tamas Vass <tamas.vass@synergysports.com>
2022-12-06 16:42:13 +0100

    Introduce tracing infra

---
+++ src/Avalonia.Base/Tracing/AvaloniaTracingEventSource.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+
+namespace Avalonia.Infrastructure.Tracing
+{
+    [EventSource(Name = "Avalonia.Win32.Tracing")]
+    public sealed class AvaloniaTracingEventSource : EventSource
+    {
+        private AvaloniaTracingEventSource()
+        {
+
+        }
+
+        [Event(1)]
+        public void Enter(string fullMethodName)
+        {
+            WriteEvent(1, fullMethodName);
+        }
+
+        [Event(2)]
+        public void Leave(string fullMethodName)
+        {
+            WriteEvent(2, fullMethodName);
+        }
+
+        [Event(3)]
+        public void Info([CallerMemberName] string message = null)
+        {
+            WriteEvent(3, message);
+        }
+
+        public static AvaloniaTracingEventSource Log { get; } = new AvaloniaTracingEventSource();
+    }
+}
---
+++ src/Avalonia.Base/Tracing/PerfTraceLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Avalonia.Infrastructure.Tracing
+{
+    public sealed class PerfTraceLogger<T> : IDisposable
+    {
+        private readonly string _method;
+
+        public PerfTraceLogger([CallerMemberName] string method = null)
+        {
+            _method = typeof(T).ToString() + "." + method;
+
+            AvaloniaTracingEventSource.Log.Enter(_method);
+        }
+
+        public void Dispose()
+        {
+            AvaloniaTracingEventSource.Log.Leave(_method);
+        }
+    }
+}
